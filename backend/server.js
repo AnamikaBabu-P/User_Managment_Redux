0401833659b5029fdcac235c962bb86e6bc66cbc
Anamika <anamikap6840@gmail.com>
2024-09-23 22:31:08 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from 'dotenv';
+dotenv.config();
+import cookieParser from "cookie-parser";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/dbConfig.js";
+const port = process.env.PORT || 3001;
+import userRoutes from './routes/userRoutes.js'
+import cors from 'cors'
+
+connectDB();
+const app= express();
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(cookieParser());
+
+app.use('/api/users', userRoutes);
+app.get('/',(req,res) => res.send('Server is ready'));
+
+app.use(notFound);
+app.use(errorHandler);
+
+
+app.listen(port, ()=> console.log(`Server started on port ${port}`))
